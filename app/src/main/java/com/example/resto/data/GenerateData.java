ede69518577cdca921acb76ec252ad5c3476e694
Andre <andre74saputra@gmail.com>
2019-10-14 21:17:19 +0700

    nambah data

--- app/src/main/java/com/example/resto/data/GenerateData.java
+++ app/src/main/java/com/example/resto/data/GenerateData.java
@@ -12,36 +12,171 @@ import static com.example.resto.model.Menu.TIPE_SNACK;
 
 public class GenerateData {
 
-	public static List<Menu> getListMenuMakanan() {
-		List<Menu> listMenuMakanan = new ArrayList<>();
+    public static List<Menu> getListMenuMakanan() {
+        List<Menu> listMenuMakanan = new ArrayList<>();
 
-		listMenuMakanan.add(new Menu(
-				R.drawable.burgerblack,
-				TIPE_MAKANAN,
-				"Burger Black",
-				"Rp. 5.000.000",
-				"Lorem"
-		));
-		listMenuMakanan.add(new Menu(
-				//wqeqweqeqweqweqweq
-		));
+        listMenuMakanan.add(new Menu(
+                R.drawable.chickenburger,
+                TIPE_MAKANAN,
+                "Chciken Burger",
+                "Rp. 34.000",
+                "dari bahan pilihan yang berkualitas baik, dengan roti kualitas terbaik, daun selada yang masih segar serta daging ayam pilihan yang berkualitas dengan baik"
+        ));
+        listMenuMakanan.add(new Menu(
+                R.drawable.burgerblack,
+                TIPE_MAKANAN,
+                "Black Burger",
+                "Rp. 38.000",
+                "sdsd"
+        ));
+        listMenuMakanan.add(new Menu(
+                R.drawable.burgerdaging,
+                TIPE_MAKANAN,
+                "Beef Burger",
+                "Rp. 36.000",
+                " dengan menggunakan daging sapi pilihan dan berkualitas serta dengan tekstur yang pas cocok untuk semua usia"
+        ));
+        listMenuMakanan.add(new Menu(
+                R.drawable.burgerkeju1,
+                TIPE_MAKANAN,
+                "Cheese Burger",
+                "Rp. 37.000",
+                "pengeluaran Burger terbaru dengan warna yang sedikit berbeda dan berkualitas baik tersedia dengan daging pilihan, keju berkualitas, dan sayuran yang terbaik dari hasil panen"
+        ));
+        listMenuMakanan.add(new Menu(
+                R.drawable.burgerspicy,
+                TIPE_MAKANAN,
+                "Spicy Burger",
+                "Rp. 40.000",
+                "sd"
+        ));
+        listMenuMakanan.add(new Menu(
+                R.drawable.hotdog,
+                TIPE_MAKANAN,
+                "Hot Dog",
+                "Rp. 24.000",
+                "d"
+        ));
+        listMenuMakanan.add(new Menu(
+                R.drawable.kebab,
+                TIPE_MAKANAN,
+                "Kebab",
+                "Rp. 26.000",
+                "sdsd"
+        ));
+        listMenuMakanan.add(new Menu(
+                R.drawable.sandwich,
+                TIPE_MAKANAN,
+                "Sandwich",
+                "Rp. 20.000",
+                "sdsd"
+        ));
 
-		return listMenuMakanan;
-	}
+        return listMenuMakanan;
+    }
 
-	public static List<Menu> getListMenuMinuman() {
-		List<Menu> listMenuMinuman = new ArrayList<>();
+    public static List<Menu> getListMenuMinuman() {
+        List<Menu> listMenuMinuman = new ArrayList<>();
 
-		// insert
+        listMenuMinuman.add(new Menu(
+                R.drawable.bubblegum1,
+                TIPE_MINUMAN,
+                "Ice Bubble Gum",
+                "Rp. 15.000",
+                "sdsd"
+        ));
+        listMenuMinuman.add(new Menu(
+                R.drawable.cappucino1,
+                TIPE_MINUMAN,
+                "Ice Cappucino",
+                "Rp. 12.000",
+                "sdsd"
+        ));
+        listMenuMinuman.add(new Menu(
+                R.drawable.taroblend1,
+                TIPE_MINUMAN,
+                "Ice Taro Blend",
+                "Rp. 16.000",
+                "sdsd"
+        ));
+        listMenuMinuman.add(new Menu(
+                R.drawable.strawberry,
+                TIPE_MINUMAN,
+                "Ice Strawberry Blend",
+                "Rp. 13.000",
+                "sdsd"
+        ));
+        listMenuMinuman.add(new Menu(
+                R.drawable.caramel,
+                TIPE_MINUMAN,
+                "Hot Carramel",
+                "Rp. 16.000",
+                "sdsd"
+        ));
+        listMenuMinuman.add(new Menu(
+                R.drawable.mochacino,
+                TIPE_MINUMAN,
+                "Hot Moccacino",
+                "Rp. 16.000",
+                "sdsd"
+        ));
+        listMenuMinuman.add(new Menu(
+                R.drawable.kopitubruk,
+                TIPE_MINUMAN,
+                "Kopi Tubruk",
+                "Rp. 10.000",
+                "sdsd"
+        ));
 
-		return listMenuMinuman;
-	}
+        return listMenuMinuman;
+    }
 
-	public static List<Menu> getListMenuSnack() {
-		List<Menu> listMenuSnack = new ArrayList<>();
+    public static List<Menu> getListMenuSnack() {
+        List<Menu> listMenuSnack = new ArrayList<>();
 
-		// insert
+        getListMenuSnack().add(new Menu(
+                R.drawable.ayamkrispi1,
+                TIPE_SNACK,
+                "Cryspi Chicken",
+                "Rp. 12.000",
+                "sdsd"
+        ));
+        getListMenuSnack().add(new Menu(
+                R.drawable.jamurkrispi1,
+                TIPE_SNACK,
+                "Jamur Crispi",
+                "Rp. 14.000",
+                "sdsd"
+        ));
+        getListMenuSnack().add(new Menu(
+                R.drawable.kentang1,
+                TIPE_SNACK,
+                "French Fries",
+                "Rp. 10.000",
+                "sdsd"
+        ));
+        getListMenuSnack().add(new Menu(
+                R.drawable.onionring1,
+                TIPE_SNACK,
+                "Onion Ring",
+                "Rp. 14.000",
+                "sdsd"
+        ));
+        getListMenuSnack().add(new Menu(
+                R.drawable.rotibakar1,
+                TIPE_SNACK,
+                "Roti Bakar",
+                "Rp. 12.000",
+                "sdsd"
+        ));
+        getListMenuSnack().add(new Menu(
+                R.drawable.tahubakso1,
+                TIPE_SNACK,
+                "Tahu Bakso",
+                "Rp. 10.000",
+                "sdsd"
+        ));
 
-		return listMenuSnack;
-	}
+        return listMenuSnack;
+    }
 }
